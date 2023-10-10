20a4534176595f6a4e8e58fb11f624d142444dfa
danner01 <92951539+danner01@users.noreply.github.com>
2023-10-10 11:46:07 -0400

    dddd

--- src/protocols/cache/local_cache.ts
+++ src/protocols/cache/local_cache.ts
@@ -6,41 +6,41 @@ export class LocalStorageProtocol implements SetStorage, GetStorage, DeleteStora
 
 
   set(key: StorageKeysEnum, value: object): void {
-    if (typeof window !== 'undefined') {
+    // if (typeof window !== 'undefined') {
       if (value) {
         localStorage.setItem(key, JSON.stringify(value))
       } else {
         localStorage.removeItem(key)
       }
-    }
+    // }
 
   }
 
   setArray(key: StorageKeysEnum, value: object[]): void {
-    if (typeof window !== 'undefined') {
+    // if (typeof window !== 'undefined') {
       if (value.length!==0) {
         localStorage.setItem(key, JSON.stringify(value))
       } else {
         localStorage.removeItem(key)
       }
-    }
+    // }
 
   }
 
   get(key: StorageKeysEnum): any {
-    if (typeof window !== 'undefined') {
+    // if (typeof window !== 'undefined') {
       const value = localStorage.getItem(key);
       if(value === '' || value === null || value === undefined) return null;
 
       return JSON.parse(localStorage.getItem(key) ?? '');
-    }
-    else return null;
+    // }
+    // else return null;
   }
 
   delete(key: StorageKeysEnum): void {
-    if (typeof window !== 'undefined') {
+    // if (typeof window !== 'undefined') {
       localStorage.removeItem(key);
-    }
+    // }
 
   }
 }
